6a2f4b4c511b8919db3993bcb505e4d1706c4353
AKAY <montypc535@gmail.com>
2026-05-30 11:22:54 +0530

    Fix recipe save to use clerkId instead of author relation

--- frontend/actions/recipe.actions.js
+++ frontend/actions/recipe.actions.js
@@ -291,7 +291,7 @@ Guidelines:
         substitutions: recipeData.substitutions,
         imageUrl: imageUrl || "",
         isPublic: true,
-        author: user.id,
+        clerkId: user.clerkId || user.id,
       },
     };
 
